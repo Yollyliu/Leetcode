c0ce4445c4b52c090e56e0dd8a2ca2640411603c
Youlin <32415716+Yollyliu@users.noreply.github.com>
2018-02-05 15:08:44 -0500

    Add files via upload

---
+++ globalLocalInversions.java
@@ -0,0 +1,52 @@
+package Week69;
+
+import java.util.Scanner;
+
+public class globalLocalInversions {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        int [] A;
+        int size=0;
+        while(scanner.next()!=""){
+            size++;
+
+        }
+        scanner.nextLine();
+        A=new int[size];
+        for(int i=0;i<size;i++){
+            A[i]=scanner.nextInt();
+        }
+
+        new globalLocalInversions().run(A);
+    }
+     public boolean run(int [] A){
+
+        int cmax=0;
+        for(int i=0;i<A.length-2;i++){
+             cmax=Math.max(cmax,A[i]);
+             if(cmax>A[i+2]) {return false;}
+
+        }
+         System.out.println("it is true");
+        return true;
+
+
+//         for(int i=0;i<A.length-1;i++){
+//             for(int j=i+1;j<A.length;j++){
+//                 if(A[i]>A[j]){
+//                     nglobal++;
+//                 }
+//             }
+//             if(A[i]>A[i+1]){
+//                 nlocal++;
+//             }
+//         }
+
+
+//        if(nglobal==nlocal){
+//            return true;
+//        }
+//        return false;
+
+    }
+}
