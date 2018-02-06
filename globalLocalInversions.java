cf7348ce49a3f2b5cb718e00974d2aa7ce9c1ed6
Youlin <32415716+Yollyliu@users.noreply.github.com>
2018-02-06 11:40:33 -0500

    Update globalLocalInversions.java

--- globalLocalInversions.java
+++ globalLocalInversions.java
@@ -1,3 +1,60 @@
+// We have some permutation A of [0, 1, ..., N - 1], where N is the length of A.
+
+// The number of (global) inversions is the number of i < j with 0 <= i < j < N and A[i] > A[j].
+
+// The number of local inversions is the number of i with 0 <= i < N and A[i] > A[i+1].
+
+// Return true if and only if the number of global inversions is equal to the number of local inversions.
+
+// Example 1:
+
+// Input: A = [1,0,2]
+// Output: true
+// Explanation: There is 1 global inversion, and 1 local inversion.
+// Example 2:
+
+// Input: A = [1,2,0]
+// Output: false
+// Explanation: There are 2 global inversions, and 1 local inversion.
+// Note:
+
+// A will be a permutation of [0, 1, ..., A.length - 1].
+// A will have length in range [1, 5000].
+// The time limit for this problem has been reduced.
+
+//Method 1:
+//Tips: the only condition local=global is that for every pair, the first> second, and elements in front pair smaller than any 
+//elements in following pairs. so we can get: max(max,A[i])<A[i+2]. For example A[1,0,3,2], A[i]< A[i+2], max(max,A[1])<A[i+2].
+
+
+//Method 2:
+//Tips: it says the permutation of[0,1,......,A.length-1], than in order to make local = global, x have three position:
+//First, it is in A[x], as there is no local or global.
+//Second, x is in A[x-1], only and only if A[x] must be x-1, then local = global =1.   x=1......A.length-1.
+//So, we can get that Math.abs(A[x]-x)>1, it will be false.
+
+
+//Sometimes, it is very hard to compute the right condition, so we should ask ourselves, "can i compute the wrong condition?"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 package Week69;
 
 import java.util.Scanner;
