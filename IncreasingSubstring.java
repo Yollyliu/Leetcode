7b24f2b36f794f24142dabbf6acbd94c23f29987
Youlin <32415716+Yollyliu@users.noreply.github.com>
2018-02-08 20:50:26 -0500

    Two ways to solve  Non-decreasing Array.

--- IncreasingSubstring.java
+++ IncreasingSubstring.java
@@ -1,3 +1,27 @@
+// Given an array with n integers, your task is to check if it could become non-decreasing by modifying at most 1 element.
+
+// We define an array is non-decreasing if array[i] <= array[i + 1] holds for every i (1 <= i < n).
+
+// Example 1:
+// Input: [4,2,3]
+// Output: True
+// Explanation: You could modify the first 4 to 1 to get a non-decreasing array.
+// Example 2:
+// Input: [4,2,1]
+// Output: False
+// Explanation: You can't get a non-decreasing array by modify at most one element.
+
+//Tips: before you do that, you should analysis all the conditions, if you ingore any part of possible condition, you will 
+//pitch up for a long time.
+
+
+
+
+
+
+
+
+
 package array;
 
 import java.util.Scanner;
