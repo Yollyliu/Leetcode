cb91c8a6c11519978b2cedf45df450c22a524c3c
Youlin <32415716+Yollyliu@users.noreply.github.com>
2018-02-10 12:59:40 -0500

    Three kind of conditions to do it.

--- PlaceFlower.java
+++ PlaceFlower.java
@@ -1,3 +1,35 @@
+// Suppose you have a long flowerbed in which some of the plots are planted and some are not. However, flowers cannot be planted in adjacent plots - they would compete for water and both would die.
+
+// Given a flowerbed (represented as an array containing 0 and 1, where 0 means empty and 1 means not empty), and a number n, return if n new flowers can be planted in it without violating the no-adjacent-flowers rule.
+
+// Example 1:
+// Input: flowerbed = [1,0,0,0,1], n = 1
+// Output: True
+// Example 2:
+// Input: flowerbed = [1,0,0,0,1], n = 2
+// Output: False
+
+
+//Tips: before coding, we should consider all the conditions, when we get the count of zero, there has three conditions.
+//1. n=count/2.  it happens when 0 is at the head or and the end of an array, for example:  0 0 0 0 1 1 0 0 0 0
+//2. n=count+1/2 it happens when there is all 0 in array, for example 0 0 0 0 0 0 
+//3. n=count-1/2 it happles when 0 betweens 1. 
+
+//After you consider all the conditions, it is easy to finish the code.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 package array;
 
 import java.util.Scanner;
