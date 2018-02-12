fa894dee635132632b07824946db4a7174fd8ace
Youlin <32415716+Yollyliu@users.noreply.github.com>
2018-02-12 12:56:08 -0500

    Three and two ways for rotate array & max profit.

--- RotateArray.java
+++ RotateArray.java
@@ -1,3 +1,28 @@
+// Three ways to rotate array, and two ways to get max profit.
+//Say you have an array for which the ith element is the price of a given stock on day i.
+
+//Design an algorithm to find the maximum profit. You may complete as many transactions as you like (ie, buy one and sell
+//one share of the stock multiple times). However, you may not engage in multiple transactions at the same time 
+//(ie, you must sell the stock before you buy again).
+
+
+//Pay attention to the last method, it is a good way to solve this porblems.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 package array;
 
 import java.util.*;
